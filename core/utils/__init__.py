31648779241b249d00e98820ea9b1e294e3715a8
ponisio7 <ponisio7@gmail.com>
2026-09-19 11:25:08 +0200

    refactor(utils): eliminar core/utils/ensamblar_html.py
    Tarea 4 del cleanup (HARNESS_TASKS.md):
    - git rm de core/utils/ensamblar_html.py (utilidad especifica de la
      calculadora: ensamblar_calculadora_html, extraer_fragmento,
      extraer_respuesta).
    - core/utils/__init__.py deja de exportar esos simbolos; mantiene
      extraer_json_de_llm.

--- core/utils/__init__.py
+++ core/utils/__init__.py
@@ -7,16 +7,8 @@ Estas utilidades pueden importarse desde el sandbox, desde tests
 unitarios y desde cualquier executor o solver.
 """
 
-from .ensamblar_html import (
-    ensamblar_calculadora_html,
-    extraer_fragmento,
-    extraer_respuesta,
-)
 from .json_llm import extraer_json_de_llm
 
 __all__ = [
-    "ensamblar_calculadora_html",
-    "extraer_fragmento",
-    "extraer_respuesta",
     "extraer_json_de_llm",
 ]
